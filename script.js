fce2f1688b81493d1e3e6794e3317420ac89c72e
Augusto <augusto.robles.mza@gmail.com>
2021-03-16 17:39:37 -0300

    developing core functions

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log(`Let's start`)
+let myLibrary = [];
+
+function Book(title,author, pages, read) {
+    this.title = title;
+    this.authot = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
+
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read))
+}
+
+function removeBookFromLibrary(title) {
+    myLibrary.filter(book => {
+        if(book.title === title) {
+            myLibrary.splice(myLibrary.indexOf(book),1)
+        } else {
+            return 'No existe tal libro'
+        }
+    })
+}
+
+console.log(myLibrary)
+
+addBookToLibrary('Job', 'Desconocido', 31, true)
+addBookToLibrary('Genesis', 'Moises', 54, true)
+addBookToLibrary('Apocalipsis', 'Juan', 25, false)
+
+
+console.log(myLibrary)
+
+removeBookFromLibrary('Genesis')
+
+console.log(myLibrary)
