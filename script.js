3fdfae1457de47e12b145d80096cc0e6ad01d6cf
Augusto <augusto.robles.mza@gmail.com>
2021-03-23 20:49:43 -0300

    Book class

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let myLibrary = [];
 
-function Book(title,author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title,author,pages,read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
+
 
 const addBookToLibrary = (title, author, pages, read) => {
     myLibrary.push(new Book(title, author, pages, read))
